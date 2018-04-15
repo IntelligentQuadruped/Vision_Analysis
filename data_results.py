4b204b9cb92cb96886b846f48ec87ef7f9f15c6a
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2018-04-15 09:59:54 -0400

    added zscore filter

--- data_results.py
+++ data_results.py
@@ -8,7 +8,21 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import seaborn
 
-data_dir = ["./data","./sample_obstacle_course"]
+def filterOutlier(data_list,z_score_threshold=3):
+	"""
+	Filters out outliers using the modified Z-Score method.
+	"""
+	# n = len(data_list)
+	# z_score_threshold = (n-1)/np.sqrt(n)
+	data = np.array(data_list)
+	median = np.median(data)
+	deviation = np.median([np.abs(x - median) for x in data])
+	z_scores = [0.675*(x - median)/deviation for x in data]
+	data_out = data[np.where(np.abs(z_scores) < z_score_threshold)].tolist()
+	output = data_out if len(data_out) > 0 else data_list
+	return output
+
+data_dir = ["./data/sample_obstacle_course"]
 # data_dir = ['./windmachine']
 
 data = []
@@ -40,10 +54,11 @@ for data_path in data_dir:
                 result = 1 - nan/total
                 data.append(result)
                 d.close()
-data.sort()
-data  = np.array(data,dtype = float)
+# data.sort()
+# data  = np.array(data)
 # delete outliers that are outside 3 sigma
-data = data[abs(data - np.mean(data)) < 3 * np.std(data)]
+# data = data[abs(data - np.mean(data)) < 3 * np.std(data)]
+data = filterOutlier(data)
 # normalize data 
 norm = len(data)
 # normed_data = np.divide(data,norm)
@@ -54,7 +69,7 @@ sigma = np.std(data)
 weights = np.ones_like(data)/float(len(data))
 
 plt.figure("depth_density")
-n,bins,patches = plt.hist(data,bins=100,weights=weights,alpha=0.75,normed=True)
+n,bins,patches = plt.hist(data,bins=100,alpha=0.75,weights=weights)
 plt.xlabel('Data Density')
 plt.ylabel('Probability of Data Density')
 plt.title('Depth Sensor Performance: n = %d, mu = %.2f, sigma = %.3f'%(len(data),mu,sigma))
